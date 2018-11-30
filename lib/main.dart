8637ee9242c4221c6646ee01c4d087557b8440b7
qinshuo <654989417@qq.com>
2018-11-30 16:47:28 +0800

    完成一个页面

--- lib/main.dart
+++ lib/main.dart
@@ -47,8 +47,7 @@ class ADemoPageState extends State<ADemoPage> {
           Container(
             width: 500.0,
             height: 150.0,
-            padding: const EdgeInsets.all(4.0),
-            margin: const EdgeInsets.all(6.0),
+            margin: const EdgeInsets.all(10.0),
             decoration: BoxDecoration(
               image: DecorationImage(
                 image:AssetImage("images/lake.jpg"),
@@ -60,7 +59,24 @@ class ADemoPageState extends State<ADemoPage> {
             ),
           ),
           _TitleSelection(title:"Oeschinen Lake Campground",subTitle:"Kandersteg, Switzerland",starCount:11),
-
+          Row(
+            //沿水平方向平均放置
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
+              _buildButtonColumn(context, Icons.call, "CALL"),
+              _buildButtonColumn(context, Icons.near_me, "ROUTE"),
+              _buildButtonColumn(context, Icons.share, "SHARE"),
+            ],
+          ),
+          Container(
+            margin: EdgeInsets.all(10.0),
+            child: Text("Lake Oeschinen lies at the foot of the bluemlisalp in the "
+                "Bernese Alps. Situated 1,578 meters above sea level, it is "
+                "one of the larger Alpine Lakes. A gondola ride from Kandersteg, "
+                "followed by a half-hour walk through pastures and pine forest, "
+                "leads you to the lake, which warms to 20 degrees Celsius in the "
+                "summer. Acitivities enjoyed here include rowing, and riding the summer toHoggan run."),
+          )
         ],
       ),
     );
@@ -121,8 +137,19 @@ class _TitleSelection extends StatelessWidget{
 Widget _buildButtonColumn(BuildContext context,IconData icon, String label){
   final color = Theme.of(context).primaryColor;
   return Column(
+    // main axis 跟我们前面提到的 cross axis 相对应，对 Column 来说，指的就是竖直方向。    
+    //  在放置完子控件后，屏幕上可能还会有一些剩余的空间（free space），min 表示尽量少占用    
+    //  free space；类似于 Android 的 wrap_content。    
+    //  对应的，还有 MainAxisSize.max    
+    mainAxisSize: MainAxisSize.min,
+    //  沿着 main axis 居中放置
+    mainAxisAlignment: MainAxisAlignment.center,
     children: <Widget>[
-      Icon(icon),
+      Icon(icon, color: color,),
+      Container(
+        margin: EdgeInsets.only(top: 7.0),
+        child: Text(label,style: TextStyle(color: color, fontSize: 13.0,),),
+      ),
     ],
   );
 }
